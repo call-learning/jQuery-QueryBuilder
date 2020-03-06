f56ade4ffe3eb0bd676c7f374fff39467493cfa9
Szabó Attila <aszabo@albacomp.hu>
2020-03-06 11:37:09 +0100

    Upgrade to Bootstrap V.4
    - New dependency: Bootstrap V.4
    - New dependency: FontAwesome V.5.12.1, Free, Web-font, Auto-Accessibility: on, Enable Version 4 Compatibility: off
    - Not necessary dependency: Bootstrap V.3
    - Fixing examples

--- src/plugins/filter-description/plugin.js
+++ src/plugins/filter-description/plugin.js
@@ -49,7 +49,7 @@ QueryBuilder.define('filter-description', function(options) {
             }
             else {
                 if ($b.length === 0) {
-                    $b = $('<button type="button" class="btn btn-xs btn-info filter-description" data-toggle="popover"><i class="' + options.icon + '"></i></button>');
+                    $b = $('<button type="button" class="btn btn-sm btn-info filter-description" data-toggle="popover"><i class="' + options.icon + '"></i></button>');
                     $b.prependTo(rule.$el.find(QueryBuilder.selectors.rule_actions));
 
                     $b.popover({
@@ -89,7 +89,7 @@ QueryBuilder.define('filter-description', function(options) {
             }
             else {
                 if ($b.length === 0) {
-                    $b = $('<button type="button" class="btn btn-xs btn-info filter-description" data-toggle="bootbox"><i class="' + options.icon + '"></i></button>');
+                    $b = $('<button type="button" class="btn btn-sm btn-info filter-description" data-toggle="bootbox"><i class="' + options.icon + '"></i></button>');
                     $b.prependTo(rule.$el.find(QueryBuilder.selectors.rule_actions));
 
                     $b.on('click', function() {
